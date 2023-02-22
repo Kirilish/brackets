47b2959c7eec2543a03d1e5f51561cf263935ad3
Kirill Semkin <93377014+Kirilish@users.noreply.github.com>
2023-02-22 14:28:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   const stack = [];
+  const openingBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+  const closingBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+  const matchingBrackets = {};
+  for (const pair of bracketsConfig) {
+    matchingBrackets[pair[1]] = pair[0];
+  }
+  for (const char of str) {
+    if (openingBrackets.has(char)) {
+      stack.push(char);
+    } else if (closingBrackets.has(char)) {
+      const matching = matchingBrackets[char];
+      if (stack.length === 0 || stack[stack.length - 1] !== matching) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
 }
